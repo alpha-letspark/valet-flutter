b8727fd36750f01036aaa5aff5d575ce1ea3417a
dipin.k <dipin.k@tahrtech.com>
2026-08-04 11:47:25 +0530

    History name updated

--- lib/HistoryScreen/View/HistoryScreen.dart
+++ lib/HistoryScreen/View/HistoryScreen.dart
@@ -36,6 +36,7 @@ class _HistoryScreenState extends State<HistoryScreen>
 
   @override
   void initState() {
+    print('This is history screeen');
     tabController = TabController(vsync: this, length: 2);
     connectionStatus = ConnectionStatusSingleton.getInstance();
     connectionStatus.initialize();
@@ -67,126 +68,233 @@ class _HistoryScreenState extends State<HistoryScreen>
             onRefreshClicked,
             onMenuClicked,
           ),
-          !isCountLoaded
-              ? const SizedBox()
-              : Column(
+          Column(
+  children: [
+    Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(width: 1.0, color: Colors.black26),
+          ),
+        ),
+        child: const ListTile(
+          title: Text(
+            Strings.HISTORY,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ),
+    ),
+
+    if (isCountLoaded)
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: DefaultTabController(
+          length: 2,
+          child: TabBar(
+            controller: tabController,
+            indicatorColor: Colors.black45,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            tabs: [
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 10,
+                    Text(
+                      Strings.TODAY,
+                      style: TextStyle(
+                        color: _currentIndex == 0
+                            ? Colors.black
+                            : Colors.black38,
+                        fontSize: 18,
                       ),
-                      child: Container(
-                          decoration: const BoxDecoration(
-                            border: Border(
-                              bottom:
-                                  BorderSide(width: 1.0, color: Colors.black26),
-                            ),
+                    ),
+                    const SizedBox(width: 10),
+                    if (todayCount != null && todayCount != "0")
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF8A8A8A),
+                          borderRadius: BorderRadius.circular(2),
+                        ),
+                        padding: const EdgeInsets.all(2),
+                        child: Text(
+                          todayCount!,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
                           ),
-                          child: const ListTile(
-                            title: Text(
-                              Strings.HISTORY,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 18),
-                            ),
-                          )),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 10,
+                        ),
                       ),
-                      child: DefaultTabController(
-                          length: 2,
-                          child: TabBar(
-                              controller: tabController,
-                              indicatorColor: Colors.black45,
-                              onTap: (index) {
-                                if (mounted) {
-                                  setState(() {
-                                    _currentIndex = index;
-                                  });
-                                }
-                              },
-                              tabs: [
-                                Tab(
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        Strings.TODAY,
-                                        style: TextStyle(
-                                            color: _currentIndex == 0
-                                                ? Colors.black
-                                                : Colors.black38,
-                                            fontSize: 18),
-                                      ),
-                                      const SizedBox(
-                                        width: 10,
-                                      ),
-                                      Container(
-                                        decoration: (todayCount == null ||
-                                                todayCount == "0")
-                                            ? const BoxDecoration()
-                                            : BoxDecoration(
-                                                color: Color(0xFF8A8A8A),
-                                                borderRadius:
-                                                    BorderRadius.circular(2),
-                                              ),
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(2.0),
-                                          child: Text(
-                                            todayCount.toString(),
-                                            style: const TextStyle(
-                                                fontWeight: FontWeight.w600,
-                                                color: Colors.white),
-                                          ),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Tab(
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        Strings.YESTERDAY,
-                                        style: TextStyle(
-                                            color: _currentIndex == 1
-                                                ? Colors.black
-                                                : Colors.black38,
-                                            fontSize: 18),
-                                      ),
-                                      const SizedBox(
-                                        width: 10,
-                                      ),
-                                      Container(
-                                        decoration: (yesterdayCount == null ||
-                                                yesterdayCount == "0")
-                                            ? const BoxDecoration()
-                                            : BoxDecoration(
-                                                color: Color(0xFF8A8A8A),
-                                                borderRadius:
-                                                    BorderRadius.circular(2),
-                                              ),
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(2.0),
-                                          child: Text(
-                                            yesterdayCount.toString(),
-                                            style: const TextStyle(
-                                                fontWeight: FontWeight.w600,
-                                                color: Colors.white),
-                                          ),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              ])),
-                    ),
                   ],
                 ),
+              ),
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      Strings.YESTERDAY,
+                      style: TextStyle(
+                        color: _currentIndex == 1
+                            ? Colors.black
+                            : Colors.black38,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    if (yesterdayCount != null && yesterdayCount != "0")
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF8A8A8A),
+                          borderRadius: BorderRadius.circular(2),
+                        ),
+                        padding: const EdgeInsets.all(2),
+                        child: Text(
+                          yesterdayCount!,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+  ],
+),
+          // !isCountLoaded
+          //     ? const SizedBox()
+          //     : Column(
+          //         children: [
+          //           Padding(
+          //             padding: const EdgeInsets.symmetric(
+          //               horizontal: 10,
+          //             ),
+          //             child: Container(
+          //                 decoration: const BoxDecoration(
+          //                   border: Border(
+          //                     bottom:
+          //                         BorderSide(width: 1.0, color: Color.fromARGB(66, 150, 109, 109)),
+          //                   ),
+          //                 ),
+          //                 child: const ListTile(
+          //                   title: Text(
+          //                     Strings.HISTORY,
+          //                     style: TextStyle(
+          //                         color: Colors.red,
+          //                         fontWeight: FontWeight.w600,
+          //                         fontSize: 18),
+          //                   ),
+          //                 )),
+          //           ),
+          //           Padding(
+          //             padding: const EdgeInsets.symmetric(
+          //               horizontal: 10,
+          //             ),
+          //             child: DefaultTabController(
+          //                 length: 2,
+          //                 child: TabBar(
+          //                     controller: tabController,
+          //                     indicatorColor: Colors.black45,
+          //                     onTap: (index) {
+          //                       if (mounted) {
+          //                         setState(() {
+          //                           _currentIndex = index;
+          //                         });
+          //                       }
+          //                     },
+          //                     tabs: [
+          //                       Tab(
+          //                         child: Row(
+          //                           mainAxisAlignment: MainAxisAlignment.center,
+          //                           children: [
+          //                             Text(
+          //                               Strings.TODAY,
+          //                               style: TextStyle(
+          //                                   color: _currentIndex == 0
+          //                                       ? Colors.black
+          //                                       : Colors.black38,
+          //                                   fontSize: 18),
+          //                             ),
+          //                             const SizedBox(
+          //                               width: 10,
+          //                             ),
+          //                             Container(
+          //                               decoration: (todayCount == null ||
+          //                                       todayCount == "0")
+          //                                   ? const BoxDecoration()
+          //                                   : BoxDecoration(
+          //                                       color: Color(0xFF8A8A8A),
+          //                                       borderRadius:
+          //                                           BorderRadius.circular(2),
+          //                                     ),
+          //                               child: Padding(
+          //                                 padding: const EdgeInsets.all(2.0),
+          //                                 child: Text(
+          //                                   todayCount.toString(),
+          //                                   style: const TextStyle(
+          //                                       fontWeight: FontWeight.w600,
+          //                                       color: Colors.white),
+          //                                 ),
+          //                               ),
+          //                             )
+          //                           ],
+          //                         ),
+          //                       ),
+          //                       Tab(
+          //                         child: Row(
+          //                           mainAxisAlignment: MainAxisAlignment.center,
+          //                           children: [
+          //                             Text(
+          //                               Strings.YESTERDAY,
+          //                               style: TextStyle(
+          //                                   color: _currentIndex == 1
+          //                                       ? Colors.black
+          //                                       : Colors.black38,
+          //                                   fontSize: 18),
+          //                             ),
+          //                             const SizedBox(
+          //                               width: 10,
+          //                             ),
+          //                             Container(
+          //                               decoration: (yesterdayCount == null ||
+          //                                       yesterdayCount == "0")
+          //                                   ? const BoxDecoration()
+          //                                   : BoxDecoration(
+          //                                       color: Color(0xFF8A8A8A),
+          //                                       borderRadius:
+          //                                           BorderRadius.circular(2),
+          //                                     ),
+          //                               child: Padding(
+          //                                 padding: const EdgeInsets.all(2.0),
+          //                                 child: Text(
+          //                                   yesterdayCount.toString(),
+          //                                   style: const TextStyle(
+          //                                       fontWeight: FontWeight.w600,
+          //                                       color: Colors.white),
+          //                                 ),
+          //                               ),
+          //                             )
+          //                           ],
+          //                         ),
+          //                       ),
+          //                     ])),
+          //           ),
+          //         ],
+          //       ),
           Visibility(
             visible: onHistoryClick,
             child: Expanded(
